955ba933dd57942420513b783220ba1e7b26942d
Fabricio Farias <fabriciorelicario@gmail.com>
2018-03-14 10:22:41 -0300

    Atualizacao do repositorio

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Books from './Books'
+import {DebounceInput} from 'react-debounce-input'
 import * as BooksAPI from '../api/BooksAPI'
 
 class SearchBooks extends Component {
@@ -15,29 +16,41 @@ class SearchBooks extends Component {
     found: []
   }
 
+  /**
+   * @description It will searh on server for the workbook
+   * @param {string} query - The term for the search
+   */
   searchBooks = query => {
-    if (query.length >= 3) {
-      BooksAPI.search(query)
-        .then(response => {
-          this.setState({ found: response });
-        })
-        .catch(error => alert("There was a problem searching the workbook."))
-    } else {
-      this.setState({ found: []});
-    }
+    BooksAPI.search(query)
+      .then(response => {
+        if (Array.isArray(response)) {
+          response.map(item => {
+            let myBook = this.props.books.find(b => b.id === item.id);
+            if (myBook) item.shelf = myBook.shelf;
+            return item;
+          });
+          this.setState({ found: response });  
+        } else {
+          this.setState({ found: []});
+        }
+      })
+      .catch(error => console.error("There was a problem searching the workbook."))
   }
 
   render() {
     
-    const { books, onChangeShelf } = this.props;
-    const { found } = this.state;
+    const { onChangeShelf } = this.props;
 
     return (
       <div className="search-books">
         <div className="search-books-bar">
            <Link className="close-search" to="/">Close</Link> 
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" onChange={(event) => this.searchBooks(event.target.value)}/>
+            <DebounceInput
+              minLength={3}
+              debounceTimeout={500}
+              placeholder="Search by title or author"
+              onChange={(event) => this.searchBooks(event.target.value)} />
           </div>
         </div>
         <div className="search-books-results">
